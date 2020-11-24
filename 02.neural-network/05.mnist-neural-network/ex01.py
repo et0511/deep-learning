442cdf81f39846cff2ea733419a1c5f18687c637
et0511 <shinwon0511@gmail.com>
2020-11-24 16:37:55 +0900

    mnist 데이터(매개변수) 구조 파악하기

--- 02.neural-network/05.mnist-neural-network/ex01.py
+++ 02.neural-network/05.mnist-neural-network/ex01.py
@@ -6,6 +6,7 @@ import sys
 import numpy as np
 from matplotlib import pyplot as plt
 from pathlib import Path
+from PIL import Image
 try:
     sys.path.append(os.path.join(Path(os.getcwd()).parent, 'lib'))
     from mnist import load_mnist
@@ -14,6 +15,20 @@ except ImportError:
 
 
 (train_x, train_t), (test_x, test_t) = load_mnist(normalize=False, flatten=True, one_hot_label=False)
+print(train_x.shape)    # 60,000 x 784 (Matrix)
+print(train_t.shape)    # 60,000 (Vector)
+
+t = train_t[0]
+print(t)                # 5
+
+x = train_x[0]
+print(x, x.shape)          # 784
+x = x.reshape(28, 28)       # 형사을 원래 이미지 크기로 변경
+print(x.shape)          # 28 x 28
+
+# 이미지 보기: PIL(Python Image Library) 사용
+pil_Image = Image.fromarray(x)
+pil_Image.show()
 
 
 
---
+++ 02.neural-network/05.mnist-neural-network/ex02.py
@@ -0,0 +1,33 @@
+# MNIST(Modified National Institude of Standard and Technology)
+# MNIST 손글씨 숫자 분류 신경망(Neural Network for MNIST Handwritten Digit): 데이터 살펴보기
+
+import os
+import sys
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+from pathlib import Path
+try:
+    sys.path.append(os.path.join(Path(os.getcwd()).parent, 'lib'))
+    from mnist import init_network, load_mnist
+except ImportError:
+    print('Library Module Can Not Found')
+
+# 1. 매개변수(w, b) 데이터 셋 가져오기
+network = init_network()
+w1, w2, w3 = network['W1'], network['W2'], network['W3']
+b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+print(w1.shape)     # 784 x 50 Matrix
+print(w2.shape)     # 50 x 100 Matrix
+print(w3.shape)     # 100 x 10 Matirx
+
+print(b1.shape)     # 50 Vector
+print(b2.shape)     # 100 Vector
+print(b3.shape)     # 10 Vector
+
+
+# 2. 학습/시험 데이터 가져오기
+(train_x, train_t), (test_x, test_t) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+
+
