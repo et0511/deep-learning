99109423d1153eeacaaf77b96950eb5e24e5e722
et0511 <shinwon0511@gmail.com>
2020-11-25 10:32:54 +0900

    신호전달: 은닉 1층 전달

--- 02.neural-network/05.mnist-neural-network/ex02.py
+++ 02.neural-network/05.mnist-neural-network/ex02.py
@@ -20,7 +20,7 @@ b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
 print(w1.shape)     # 784 x 50 Matrix
 print(w2.shape)     # 50 x 100 Matrix
-print(w3.shape)     # 100 x 10 Matirx
+print(w3.shape)     # 100 x 10 Matrix
 
 print(b1.shape)     # 50 Vector
 print(b2.shape)     # 100 Vector
@@ -29,5 +29,20 @@ print(b3.shape)     # 10 Vector
 
 # 2. 학습/시험 데이터 가져오기
 (train_x, train_t), (test_x, test_t) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+
+xlen = len(test_x)
+randidx = np.random.randint(0, xlen, 1).reshape(())
+
+# 3. 신호전달
+print('\n== 신호전달 구현1: 은닉 1층 전달 ====================')
+
+x = test_x[randidx]
+print(f'x dimension: {x.shape}')        # 784 vector
+
+w1 = network['W1']
+print(f'w1 dimension: {w1.shape}')      # 784 x 50 matrix
+
+b1 = network['b1']
+print(f'b1 dimension: {b1.shape}')      # 50 vector
 
 
