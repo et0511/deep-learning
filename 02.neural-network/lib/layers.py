3d71a377331a412d6cda9b862e12ac50b1d30993
et0511 <shinwon0511@gmail.com>
2020-12-01 16:06:32 +0900

    backpropagation Layer 기반 기울기 구하기 구현 및 테스트

--- 02.neural-network/lib/layers.py
+++ 02.neural-network/lib/layers.py
@@ -9,12 +9,40 @@ try:
 except ImportError:
     print('Library Module Can Not Found')
 
+# Affine Layer
+class Affine:
+    def __init__(self, w, b):
+        self.x = None
+
+        self.w = w
+        self.b = b
+
+        self.dw = None
+        self.db = None
+
+    def forward(self, x):
+        # 역전파시 전치핼렬에 대한 내적을 대비
+        if x.ndim == 1:
+            x = x[np.newaxis, :]
+
+        self.x = x
+        out = np.dot(self.x, self.w) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.w.T)
+        self.dw = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
 
 # SoftmaxWithLoss Layer
 class SoftmaxWithLoss:
     def __init__(self):
-        self.x = None
         self.y = None
+        self.t = None
 
     def forward(self, x, t=None):
         self.y = softmax(x)
@@ -37,7 +65,7 @@ class SoftmaxWithLoss:
         return dx
 
 
-class Sogmoid:
+class Sigmoid:
     def __init__(self):
         self.out = None
 
